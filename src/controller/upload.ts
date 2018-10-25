cd08b05b33b5bb5331c4e93768d930deef9fd7f5
tangxiaopeng <630787976@qq.com>
2018-10-25 17:53:12 +0800

    10-25

--- src/controller/upload.ts
+++ src/controller/upload.ts
@@ -1,4 +1,5 @@
 import Base from './base.js';
+import * as urllib from 'urllib'
 export default class extends Base {
   async indexAction() {
     if(!this.ctx.isPost){
@@ -6,11 +7,24 @@ export default class extends Base {
             success: false
         })
     }
-    let themefile = this.file()
     const res = await this.upload()
-    // this.ctx.json(this.ctx.config('upload'))
-    // this.ctx.json({themefile})
-    console.log(res)
-    this.ctx.json({res})
+    const callback = this.get('callback')
+    let ret = {status: true, data: res}
+    if(callback){
+      const result = await urllib.request(callback, {
+        method: 'GET',
+        data: {data: JSON.stringify(res)},
+        dataType: 'json'
+      })
+      ret.data = result.data && result.data.data
+    }
+    this.ctx.json(ret)
+  }
+  async demoAction(){
+    const str: string = this.get('data')
+    const data: any = JSON.parse(str)
+    let i = 1
+    data.forEach((it: any) => it.id = i++)
+    return this.ctx.json({data})
   }
 }
